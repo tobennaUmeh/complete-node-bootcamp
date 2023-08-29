cebba6d953d600fbbbec9dfa542dfdc6d1ce61f2
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2023-08-29 12:33:37 +0100

    App initialised

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// const colors = require('tailwindcss/colors')
+
+module.exports = {
+	content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1E1E1E',
+			},
+		},
+	},
+	plugins: [],
+};
