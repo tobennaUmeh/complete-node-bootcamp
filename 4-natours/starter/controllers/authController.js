ac2d30312faf3a5aa94086736695f21cae8b4d40
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2024-09-07 21:51:39 +0100

    auth roles added

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -182,7 +182,7 @@ exports.restrictTo = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new AppError(
-          'You do not have permission to perform this action',
+          `You do not have permission to perform this action only ${roles} can perform this action`,
           403
         )
       );
